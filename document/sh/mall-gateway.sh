0781a6d7312ef7407e35b75f7e2b31a26d03cd99
huanghaixin <huanghaixin@baikemy.com>
2020-04-19 21:34:03 +0800

    修改sh

--- document/sh/mall-gateway.sh
+++ document/sh/mall-gateway.sh
@@ -7,7 +7,6 @@ echo '----rm container----'
 docker rmi `docker images | grep none | awk '{print $3}'`
 echo '----rm none images----'
 docker run -p 8201:8201 --name ${app_name} \
---link mall-registry:mall-registry \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
 -d mall/${app_name}:1.0-SNAPSHOT

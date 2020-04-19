919e5948de946e8930bfdf08f8900aab825ccd0d
huanghaixin <huanghaixin@baikemy.com>
2020-04-19 20:32:05 +0800

    修改sh

--- document/sh/mall-admin.sh
+++ document/sh/mall-admin.sh
@@ -7,8 +7,6 @@ echo '----rm container----'
 docker rmi `docker images | grep none | awk '{print $3}'`
 echo '----rm none images----'
 docker run -p 8180:8180 --name ${app_name} \
---link mysql:db \
---link mall-registry:mall-registry \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
 -d mall/${app_name}:1.0-SNAPSHOT
